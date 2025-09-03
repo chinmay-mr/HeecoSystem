0678c62f2f4f5f93777cd31a2f6083a6a8452828
chinmay-mr <chinmaymr29@gmail.com>
2025-09-03 16:17:39 +0530

    task modification view

--- Doctor/urls.py
+++ Doctor/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import PatientCreateAPIView,PatientRetrieveUpdateView,TaskCreateAPIView
+from .views import PatientCreateAPIView,PatientRetrieveUpdateView,TaskCreateAPIView,TaskModifyAPIView
 urlpatterns = [
     path('patients/',PatientCreateAPIView.as_view(),name='ListCreate-patients'),
     path('patients/<int:id>/',PatientRetrieveUpdateView.as_view(),name='retrieveUpdate-patients'),
     path('patients/<int:patient_id>/tasks/',TaskCreateAPIView.as_view(),name='create-list-tasks'),
+    path('patients/<int:patient_id>/tasks/<int:id>',TaskModifyAPIView.as_view(),name='modify-tasks'),
 
 
 ]
--- Doctor/views.py
+++ Doctor/views.py
@@ -26,7 +26,21 @@ class TaskCreateAPIView(generics.ListCreateAPIView):
         return Tasks.objects.filter(patient_id=patient_id)
     
     def perform_create(self,serializer):            # saves the patient field of tasks model
-        patient=get_object_or_404(Patients,id=self.kwargs['patient_id']) #check it once
+        patient=get_object_or_404(Patients,id=self.kwargs['patient_id']) 
         serializer.save(patient=patient)
+
+
+# Update Delete Retrive specific task of a patient
+class TaskModifyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class=TaskSerializer
+    lookup_field='id'
+
+    def get_queryset(self):
+        tasks_of_specific_patient=Tasks.objects.filter(patient=self.kwargs['patient_id'])
+        return tasks_of_specific_patient
+    
+
+
+
 
     
